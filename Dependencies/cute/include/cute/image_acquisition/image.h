421a280e2b7db3e03b28bd1de5bc108f09c0a5f1
ali-samer <samertiz940@gmail.com>
2023-08-10 22:11:00 -0700

    update dependency; extend CUTE facilities

--- Dependencies/cute/include/cute/image_acquisition/image.h
+++ Dependencies/cute/include/cute/image_acquisition/image.h
@@ -1,88 +1,141 @@
-/** @brief Retrieving images from the disk
+/** @brief Retrieving and decoding images from the disk
  * @author Samer Ali
  * @data August 7, 2023
-**/
+ **/
+
+/*
+	The MIT License (MIT)
+	
+	Copyright (c) 2014-2018 Omar Cornut
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+ */
 
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image.h"
-#include "stb_image_write.h"
-#include <utility>
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+
+//#include "stb_image.h"
+//#include "stb_image_write.h"
 #include <cstdio>
 #include <cstdlib>
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <malloc/malloc.h>
+#include <cute/utility.h>
+#include <dirent.h>
+#include <vector>
+#include <cassert>
+#include "filesystem"
+#include "stb_image.h"
 
-#define RGBA 4
 
 struct Image
 {
-    uint8_t* pixels { NULL };
-    GLuint texture;
-    int texture_loaded {};
-    int width;
-    int height;
-    int num_color_ch;
+	//	GLenum texture_internal_fmt;
+	//	GLenum original_pixel_fmt;
+	//	GLenum texture_data_t;
+	//	GLenum original_data_t;
+	GLuint texture;
+	uint8_t* pixels { NULL };
+	
+	int texture_loaded { };
+	
+	int width;
+	int height;
+	int num_color_ch;
+	//	int  num_mipmap_lvls;
+	//
+	//	bool flip_vertically;
+	//	int  error_code;
+	//	const char* error_msg;
 };
+void load_image ( Image* img ,
+                  const char* path );
+void free_image ( Image* img );
+void reset_image ( Image* img );
+void texture_image ( Image* img );
+void reset_texture_image ( Image* img );
 
 
-void free_image(Image* img) {
-
-    // Validate input
-    if(!img || !img->pixels) return;
-
-    // Free pixel data
-    stbi_image_free(img->pixels);
-
-    // Clear pointer
-    img->pixels = NULL;
-
-    // Reset other fields
-    img->width = 0;
-    img->height = 0;
-    img->num_color_ch = 0;
-
+void load_image ( Image* img ,
+                  const char* path )
+{
+	using namespace cute;
+	if ( !cute::is_path( path ) )
+		//		ERROR_EXIT( "Path does not exist. Unable to load image." );
+		
+		reset_image( img );
+	img->pixels = stbi_load( path , &img->width , &img->height , &img->num_color_ch , RGBA );
+	texture_image( img );
 }
 
-void reset_image(Image* img) {
 
-    // Free existing pixel data if present
-    if(img->pixels) {
-        stbi_image_free(img->pixels);
-        img->pixels = NULL;
-    }
-
-    // Delete texture if present
-    if(img->texture_loaded) {
-        glDeleteTextures(1, &img->texture);
-        img->texture_loaded = false;
-    }
-
-    // Reset other fields
-    img->width = 0;
-    img->height = 0;
-    img->num_color_ch = 0;
-
+void free_image ( Image* img )
+{
+	if ( !img || !img->pixels )
+		return;
+	stbi_image_free( img->pixels );
+	img->pixels = NULL;
 }
 
-void texture_image(Image* img) {
 
-    // Generate texture handle
-    glGenTextures(1, &img->texture);
-
-    // Bind the texture
-    glBindTexture(GL_TEXTURE_2D, img->texture);
-
-    // Set filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // Upload pixel data
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->width, img->height, 0,
-                 GL_RGBA, GL_UNSIGNED_BYTE, img->pixels);
-
-    // Set flag indicating texture is available
-    img->texture_loaded = true;
-
+void reset_image ( Image* img )
+{
+	free_image( img );
+	reset_texture_image( img );
+	img->width        = 0;
+	img->height       = 0;
+	img->num_color_ch = 0;
+	//	img->texture_internal_fmt = GL_RGBA;
+	//	img->texture_data_t       = GL_UNSIGNED_BYTE;
+	//	img->num_mipmap_lvls      = 0;
+	//	img->original_pixel_fmt   = GL_RGBA;
+	//	img->original_data_t      = GL_UNSIGNED_BYTE;
+	//	img->flip_vertically      = false;
+	//	img->error_code           = 0;
+	//	img->error_msg            = nullptr;
 }
+
+
+void texture_image ( Image* img )
+{
+	if ( img->pixels == NULL )
+		assert( false && "Image data is NULL" );
+	glGenTextures( 1 , &img->texture );
+	glBindTexture( GL_TEXTURE_2D , img->texture );
+	glTexParameteri( GL_TEXTURE_2D , GL_TEXTURE_MIN_FILTER , GL_LINEAR );
+	glTexParameteri( GL_TEXTURE_2D , GL_TEXTURE_MAG_FILTER , GL_LINEAR );
+	
+	glTexImage2D( GL_TEXTURE_2D , 0 , GL_RGBA , img->width , img->height , 0 , GL_RGBA , GL_UNSIGNED_BYTE ,
+	              img->pixels );
+	
+	glGenerateMipmap( GL_TEXTURE_2D );
+	img->texture_loaded = true;
+}
+
+
+void reset_texture_image ( Image* img )
+{
+	if ( img->texture_loaded )
+		glDeleteTextures( 1 , &img->texture );
+	img->texture_loaded = false;
+}
+
+
--- src/testCode.cpp
+++ src/testCode.cpp
@@ -1,11 +1,215 @@
+#include "cute/image_acquisition/image.h"
 #include "cynthia-perception/core.h"
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <cute/utility.h>
 
-int main(int, char**)
+
+int fs ( std::string& s ,
+         std::string& cd ,
+         const char* popup )
 {
-    cy::mat window("Test Window", 800, 600);
-    if(window.create_window())
-    {
-        window.run();
-    }
-    return 0;
+	static std::string current_dir = ".";
+	if ( !ImGui::BeginPopup( popup ) )
+		return -1;
+	
+	int                        sindex = -1;
+	std::vector< std::string > dlist  = cute::directory_list( current_dir.c_str() , cute::FILES_AND_DIRECTORIES );
+	ImGui::Text( "Files" );
+	static ImGuiTextFilter filter;
+	filter.Draw( "" );
+	
+	for ( int i = 0 ; i < dlist.size() ; i++ )
+	{
+		if ( filter.PassFilter( dlist[ i ].c_str() ) )
+		{
+			if ( ImGui::Selectable( dlist[ i ].c_str() ) )
+			{
+				if ( cute::is_directory( ( current_dir + "/" + dlist[ i ] ).c_str() ) )
+				{
+					current_dir += "/" + dlist[ i ];
+					filter.Clear();
+				} else
+				{
+					sindex = i;
+					s      = dlist[ i ];
+					filter.Clear();
+				}
+				cd = current_dir;
+				break;
+			}
+		}
+	}
+	ImGui::EndPopup();
+	if ( sindex == -1 )
+		ImGui::OpenPopup( popup );
+	
+	return sindex;
+}
+
+
+//pops up the file list
+std::string image_select ( int s )
+{
+	static std::vector< std::string > selected = { "" ,
+	                                               "" };
+	ImGui::SameLine();
+	ImGui::Text( selected[ s ] == "" ? "<None>" : selected[ s ].c_str() );
+	
+	std::string current_dir;
+	int         sindex;
+	if ( s == 0 )
+		sindex = fs( selected[ s ] , current_dir , "select 0" );
+	else if ( s == 1 )
+		sindex = fs( selected[ s ] , current_dir , "select 1" );
+	
+	if ( sindex == -1 )
+		return "";
+	else
+		return current_dir + "/" + selected[ s ];
+}
+
+// std::string image_select() {
+//     // Create a file dialog.
+//     std::string filename;
+//     ImGui::FileOpenDialog("Image Select", filename, "All Files (*.*);;Image
+//     Files (*.png, *.jpg, *.jpeg)", ImGuiFileDialogFlags_NoChangeDir);
+//
+//     // If the user selected a file, return the filename.
+//     if (ImGui::FileOpenDialog::IsOpened("Image Select")) {
+//         return filename;
+//     }
+//
+//     // If the user did not select a file, return an empty string.
+//     return "";
+// }
+
+// std::string open_dialog() {
+//     std::string filePath;
+//
+//     GLFWwindow* window = glfwGetCurrentContext();
+//     if (window) {
+//         // Open the file dialog
+//         const char* selectedPath = glfwOpenWindowFile(window, "Open File",
+//         nullptr, nullptr, nullptr);
+//
+//         if (selectedPath) {
+//             // File selected
+//             filePath = selectedPath;
+//         }
+//     } else {
+//         // Unable to get the current GLFW context
+//     }
+//
+//     return filePath;
+// }
+
+ImVec2 resize_ui ( int width ,
+                   int height ,
+                   int mul )
+{
+	return ImVec2( width * mul , height * mul );
+}
+
+
+int main ( int ,
+           char** )
+{
+	glfwSetErrorCallback( cute::error_callback );
+	if ( !glfwInit() )
+	{
+		return 1;
+	}
+	
+	//! OpenGL version 3
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR , 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR , 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE , GLFW_OPENGL_CORE_PROFILE );
+	//! For Mac Users
+	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT , GLFW_TRUE );
+	
+	//! Create Window and init glad
+	GLFWwindow* window = glfwCreateWindow( 800 , 600 , "Increment" , NULL , NULL );
+	glfwMakeContextCurrent( window );
+	gladLoadGLLoader( ( GLADloadproc ) glfwGetProcAddress );
+	
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	//    ImGuiIO &io = ImGui::GetIO();
+	
+	//! Setup ImGui binding
+	ImGui_ImplGlfw_InitForOpenGL( window , true );
+	ImGui_ImplOpenGL3_Init( "#version 400" );
+	ImGui::StyleColorsDark();
+	
+	Image  img;
+	ImVec2 dim( img.width , img.height );
+	ImVec4 clear_color = ImColor( 144 , 144 , 154 );
+	int    x           = 0;
+	while ( !glfwWindowShouldClose( window ) )
+	{
+		glfwPollEvents();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		
+		ImGui::Begin( "Slider Window" );
+		if ( img.texture_loaded )
+			ImGui::Image( ( void* ) img.texture , ImVec2( img.width , img.height ) );
+		
+		if ( ImGui::Button( "Select Image" ) )
+		{
+			ImGui::OpenPopup( "select-0" );
+		}
+		
+		std::string tmp = image_select( 0 );
+		
+		if ( tmp != "" )
+		{
+			load_image( &img , tmp.c_str() );
+			//			free_image( &img );
+		}
+		
+		
+		ImGui::End();
+		
+		int display_w , display_h;
+		glfwGetFramebufferSize( window , &display_w , &display_h );
+		glViewport( 0 , 0 , display_w , display_h );
+		glClearColor( clear_color.x , clear_color.y , clear_color.z , clear_color.w );
+		glClear( GL_COLOR_BUFFER_BIT );
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
+		glfwSwapBuffers( window );
+	}
+	
+	//    while(!glfwWindowShouldClose(window))
+	//    {
+	//        glfwPollEvents();
+	//        ImGui_ImplOpenGL3_NewFrame();
+	//        ImGui_ImplGlfw_NewFrame();
+	//        ImGui::NewFrame();
+	//
+	//        ImGui::Begin("counter");
+	//        if (ImGui::Button("CLICK ME"))
+	//            x++;
+	//        ImGui::Text("X: %d", x);
+	//        ImGui::End();
+	//
+	//        int display_w, display_h;
+	//        glfwGetFramebufferSize(window, &display_w, &display_h);
+	//
+	//        glViewport(0, 0, display_w, display_h);
+	//        glClearColor(clear_color.x, clear_color.y, clear_color.z,
+	//        clear_color.w); glClear(GL_COLOR_BUFFER_BIT);
+	//
+	//        ImGui::Render();
+	//        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	//        glfwSwapBuffers(window);
+	//    }
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwTerminate();
+	return 0;
 }
